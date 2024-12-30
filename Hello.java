a09e4049519a0fe007c34c8cdcdf577368e45c07
OMIT HASAN <omithasan68479@gmail.com>
2024-12-30 22:11:40 +0600

    first commit

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(4+5);
+        System.out.println(4-5);
+        System.out.println(4-5*9);
+        System.out.println(3/7);
+    }
+}
