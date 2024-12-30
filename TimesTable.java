78ee4e74ff2a442ce36efd177b40d75509074545
OMIT HASAN <omithasan68479@gmail.com>
2024-12-30 22:12:17 +0600

    first commit

---
+++ TimesTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TimesTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        System.out.println(number + " times table: ");
+        int counter = 1;
+        while (counter <= 10) {
+            System.out.println(number +" X " + counter + " = " + (counter * number));
+            counter++;
+        }
+    }
+}
